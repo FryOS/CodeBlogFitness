083b0d0e65fd47526ee0a8d3b9b92bae3a399760
Alexey <alexeyosp@gmail.com>
2019-06-23 12:32:42 +0300

    Models

---
+++ CodeBlogFitness.BL/Models/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBlogFitness.BL.Models
+{
+    /// <summary>
+    /// Gender
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Summary
+        /// </summary>
+        
+        public String Name { get; }
+        /// <summary>
+        /// Create new gender
+        /// </summary>
+        /// <param name="name">Name of gender</param>
+        public Gender(string name) 
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CodeBlogFitness.BL/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBlogFitness.BL.Models
+{
+    public class User
+    {
+        public string Name { get; }
+
+        public Gender Gender { get; }
+
+        public DateTime BirthDate { get; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public User(string name, 
+                    Gender gender, 
+                    DateTime birthDate, 
+                    double weight, 
+                    double height)
+        {
+            //if(string.)
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+
+        }
+    }
+}
